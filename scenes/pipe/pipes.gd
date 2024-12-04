5d667ee34731fe50914de0a6e230bcc50a0c4a0d
David Zywiec <dzlast95@gmail.com>
2024-12-04 16:00:07 -0600

    Added global scroll speed.

--- scenes/pipe/pipes.gd
+++ scenes/pipe/pipes.gd
@@ -2,15 +2,19 @@ extends Node2D
 
 class_name pipes
 
-const SPEED = 120;
+const OFF_SCREEN_X : float = -500.0
 @onready var oscn : VisibleOnScreenNotifier2D = $OnScreenNotify
 
 func _ready():
 	oscn.connect("screen_exited",exit_screen)
 
+func _process(delta: float) -> void:
+	if position.x <= OFF_SCREEN_X:
+		exit_screen()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	position.x -= SPEED * delta;
+	position.x -= GameManager.SCROLL_SPEED * delta;
 
 func exit_screen():
 	queue_free()
